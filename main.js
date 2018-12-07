aeaf09785f87ae0acfd8eea692f66710fd7002d6
VenkatZen <45593025+VenkatZen@users.noreply.github.com>
2018-12-07 10:57:57 +0530

    final 1

---
+++ main.js
@@ -0,0 +1,16 @@
+var fhalf=document.getElementById('firsthalf');
+function changename()
+{
+    var uname=prompt('enter your name');
+    localStorage.setItem('name',uname);
+    fhalf.textContent='welcome to FLB '+uname;
+}
+if(localStorage.getItem('name'))
+{
+    changename();
+}
+else{
+    var setname=localStorage.getItem('name');
+    fhalf.textContent='welcome to FLB '+setname;
+}
+
